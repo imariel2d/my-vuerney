897741221ca49e2691ea2db5a8eccf8ac2eb3d03
Ariel Plascencia <imariel2d@gmail.com>
2023-02-04 12:54:02 -0700

    creating vue app -> counter app

---
+++ 000-counter/src/App.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="counter">
+    <button @click="increment">+1</button>
+    <h1>{{ count }}</h1>
+    <button @click="decrement">-1</button>
+  </div>
+</template>
+
+<style scoped>
+  #counter {
+    display: flex;
+    gap: 2rem;
+  }
+
+  #counter button {
+    border-radius: 1rem;
+    border: 1px solid black;
+    cursor: pointer;
+  }
+</style>
